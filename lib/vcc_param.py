9af127354272b85f474206b86c2f117657520c93
fpemud <fpemud@sina.com>
2017-09-12 22:57:48 +0800

    fix

--- lib/vcc_param.py
+++ lib/vcc_param.py
@@ -15,20 +15,3 @@ class VccParam:
 			self.dataDir = "/var/cache/config-sync"
 		else:
 			self.dataDir = "/home/%s/.cache/config-sync" % (pwd.getpwuid(os.getuid())[0]))
-
-        if os.getuid() == 0:
-            self.watchPathList = {
-                "/etc": [
-                    "skel",					# directories
-                    "ssl/certs",
-                    "mtab",					# files
-                    "UTC",
-                ]
-                "/var/lib/portage/world",
-            }
-            self.dataDir = "/var/cache/fpemud-vcc"
-        else:
-            self.watchPathList = {
-                
-            }
-            self.dataDir = "~/.cache/fpemud-vcc"
